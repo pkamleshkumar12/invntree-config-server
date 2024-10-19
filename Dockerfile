773c681d82c826fc47a32551f19efcfd39cc6584
kamlesh <pkamleshkumar12@gmail.com>
2024-10-19 22:01:35 +1300

    updated application yaml for local and minikube

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM openjdk:23
 LABEL authors="kamlesh"
 VOLUME /tmp
 COPY /target/config-server-0.0.1-SNAPSHOT.jar app.jar
-ENTRYPOINT ["java", "-jar", "/app.jar"]
+
+ENTRYPOINT ["java", "-jar", "/app.jar", "--spring.profiles.active=minikube"]
--- src/main/java/com/invntree/configserver/InvntreeConfigServerApplication.java
+++ src/main/java/com/invntree/configserver/InvntreeConfigServerApplication.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
+//@EnableConfigServer
 public class InvntreeConfigServerApplication implements CommandLineRunner {
 
     private final VaultConfig vaultConfig;
